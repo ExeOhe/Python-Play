5e31c5e480fe59c0db476e68eb184b69b8cc984f
Jecob Snyder <x0jxc0b315@gmail.com>
2025-04-18 17:08:21 -0400

    added digital clock data

--- Digital_Clock.py
+++ Digital_Clock.py
@@ -1,10 +1,17 @@
 import datetime
 import tkinter as tk
+import os
 
 def update_time():
     current_time = datetime.datetime.now().strftime("The time is currently : %I:%M:%S %p")
     time_label.config(text=current_time)
     root.after(1000, update_time)  # Update the time every second
+
+def check_repo():
+    if os.path.isdir(".git"):
+        print("This directory is a git repository.")
+    else:
+        print("This directory is not a git repository.")
 
 # Create the main window
 root = tk.Tk()
@@ -16,6 +23,9 @@ time_label.pack()
 
 # Call the update_time function to initialize the display
 update_time()
+
+# Check if there is a repository for this file
+check_repo()
 
 # Start the Tkinter event loop
 root.mainloop()
