e452e358d4f7bc9195868aa0f0b46aa073358799
Jecob Snyder <x0jxc0b315@gmail.com>
2025-05-07 15:44:46 -0400

    Main.py est. Have Fun Dev's :)

---
+++ main.py
@@ -0,0 +1,7 @@
+DevReq = input("Hellow art thou any devs?")
+if "Y, y" in DevReq:
+    print("I KNEW IT AYAYAYAAYAH")
+elif "N, n" in DevReq:
+    print("YOUGHS A LIGHAER!!!")
+else:
+    print("I AM NOT A DEV BUT I AM A GIT REPO")
