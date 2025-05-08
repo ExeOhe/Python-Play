d53a9ed8fdbe83c11218570882e746e35f1ca763
Jecob Snyder <x0jxc0b315@gmail.com>
2025-05-08 18:37:12 -0400

    Add:Equality_to_100.py script, add:Comment to main.py portfolio script description

---
+++ equality_to_100.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a # to test its equality to 100: "))
+if n >100:
+    print(n, ":Your # is greater than 100!")
+elif n < 100:
+    print(n, ":Your # is less than 100!")
+elif n == 100:
+    print(n, ":Your # is = 100!")
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Reusable Function for Portfolio "Feeling Questionare"
 DevReq = input("Hellow art thou any devs?")
 if "Y" in DevReq or "y" in DevReq:
     print("I KNEW IT AYAYAYAAYAH")
