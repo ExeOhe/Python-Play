97fafc8c243d9004b6d0e0bfbfcc3dd187a8da72
Jecob Snyder <x0jxc0b315@gmail.com>
2025-05-07 20:13:18 -0400

    created:feelinggreeting for portfolio

---
+++ Feeling_Greeting.py
@@ -0,0 +1,12 @@
+#Define Emotions for Greeting to Response
+goodfeel = ["good", "excited", "great", "happy", "fantastic", "amazing"]
+badfeel = ["bad", "sad", "upset", "angry","tired","exausted","depressed","mad"]
+
+feeling= input("how are you feeling?")
+for word in goodfeel:
+    if word in feeling:
+     print("Hurray ! Im stoked that youre feeling", feeling)
+for word in badfeel:
+    if word in feeling:
+     print("I'm so sorry you're going through this", feeling)
+     break
